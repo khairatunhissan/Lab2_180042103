db5905b9734adcbb9ed64819c8d889fc1393cfd6
Khairatun Hissan <khairatunhissan@gmail.com>
2021-06-28 12:42:57 +0600

    mongodb connected successfully

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const app = require('./app');
 
 require('dotenv').config();
+const mongoose=require('mongoose')
+
+mongoose.connect(process.env.MONGOURL,{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useUnifiedTopology:true,
+})
+.then(()=>{
+    console.log('Mongodb Connected')
+})
+.catch((err)=>{
+    if(err){
+        console.log('unable to connect to mongodb')
+    }
+})
 
 const PORT = process.env.PORT;
 
