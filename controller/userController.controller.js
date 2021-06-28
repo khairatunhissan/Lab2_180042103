3e6401c54255ac0f03da362b4bcf731cb6ba98dc
Khairatun Hissan <khairatunhissan@gmail.com>
2021-06-28 16:37:45 +0600

    User can be now stored and hashing done

--- controller/userController.controller.js
+++ controller/userController.controller.js
@@ -1,4 +1,6 @@
 const User=require('../models/userModel')
+const bcrypt=require('bcryptjs')
+const alert= require('alert')
 
 const  getLoginPage =(req,res) => {
     res.sendFile('login.html', {root: './views/pages/examples'});
@@ -8,32 +10,52 @@ const  getRegisterPage =(req,res) => {
     res.sendFile('register.html', {root: './views/pages/examples'});
 };
 
-const postRegisterPage =(req,res)=>{
+const postRegisterPage =async(req,res)=>{
     const name=req.body.name
     const email=req.body.email
     const password=req.body.password
     const retype=req.body.retype
 
     try{
-        const user=User.findOne({email:email})
+        const user=await User.findOne({email:email})
         if(user){
             alert("There is already an account under this user")
-            res.redirect('./login')
+            res.redirect('/login')
         }
         else if(password.length<=6){
             alert('Password must contain 6 characters')
-            res.redirect('./register')
+            res.redirect('/register')
         }
+        else if(password!==retype){
+            alert('Please enter same password')
+            res.redirect('/register')
+        }
+        else if(!name||!email){
+            alert("please fill the fields")
+            res.redirect('/register')
+        }
+        else{
+            const salt=await bycrypt.genSaltSync(10)
+            const passwordHash=await bycrypt.hash(password,salt)
+            const createUser=new User({
+                name,
+                email,
+                passwordHash,
+            })
+            await createUser.save()
+            alert('Sucessfully Created')
+            res.redirect('/login')
+        }
+
 
     }
     catch(error){
+        console.log(error)
+        alert('Something went wrong')
+        res.redirect('/register')
 
     }
 
-    if(password.length<=6||!name||!email){
-        return res.json({message:'Registration Failed'})
-    }
-    res.json({message:'Registration Succesfull'})
 }
 
 module.exports ={
